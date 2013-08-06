f4d93399d2e3a25a475e87b4398695432051fe17
rja <rja@diotasoft.com>
2013-08-06 12:28:48 +0200

    initialCommit

---
+++ Script.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Script : MonoBehaviour {
+	
+	IEnumerator Start () {
+		yield return null;
+	
+	}
+	
+
+	void Update () {
+	
+	
+			
+	}
+}
